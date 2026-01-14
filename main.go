0fab3250f53e41d653b52a7e01fcd0cb4a9c249e
Zahar Putincev <zakhar-0@mail.ru>
2026-01-14 17:24:12 +0300

    add simple database connection

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"study_3/feature1"
 	"study_3/feature2"
+	"study_3/feature_postgres/simple_connection"
 )
 
 
@@ -12,4 +13,5 @@ func main()  {
 	fmt.Println("Hello, Git!")
 	feature1.Feature1()
 	feature2.Feature2()
+	simple_connection.CheckConnection()
 }
